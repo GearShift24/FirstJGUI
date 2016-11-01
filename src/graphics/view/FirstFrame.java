b3b7372823b3a6c7b776b60139dbbfb4d7e25ecb
GearShift24 <jwri6847@740s-pl-369.local>
2016-11-01 08:15:02 -0600

    created frame and Frame constructor

---
+++ src/graphics/view/FirstFrame.java
@@ -0,0 +1,17 @@
+package graphics.view;
+
+import javax.swing.JFrame;
+import graphics.controller.FirstGUIController;
+
+public class FirstFrame extends JFrame
+{
+	private FirstGUIController baseController;
+	
+	public FirstFrame(FirstGUIController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		
+		
+	}
+}
