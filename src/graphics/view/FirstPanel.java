b121da33747241cc3ae51d990b3c318647c78221
GearShift24 <jwri6847@740s-pl-369.local>
2016-11-01 09:10:24 -0600

    added layout listeners and panel. Along with changing color buttons

--- src/graphics/view/FirstPanel.java
+++ src/graphics/view/FirstPanel.java
@@ -2,22 +2,52 @@ package graphics.view;
 
 import javax.swing.JPanel;
 import graphics.controller.FirstGUIController;
+import java.awt.Color;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstPanel extends JPanel
 {
 	private FirstGUIController baseController;
-	
+	private JButton colorButton;
+	private JButton randomButton;
+
 	public FirstPanel(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
+
+		colorButton = new JButton("Color!");
+		randomButton = new JButton("Random");
+
 		setupPanel();
-		
+		setupLayout();
+		setupListeners();
 	}
-	
+
 	private void setupPanel()
 	{
-		
+		this.setBackground(Color.magenta);
+		this.add(colorButton);
+		this.add(randomButton);
 	}
+
+	private void setupLayout()
+	{
+
+	}
+
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.gray);
+			}
+		});
+	}
+
 }
